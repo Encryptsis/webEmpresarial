5cc784ffc2131964b2a60d09e497ae9603fb67aa
David Suchiapa <davidsuchiapa@gmail.com>
2026-03-05 10:54:58 -0600

    Se modificaron URL, portfolio.html y ahora se puede agregar proyectos a portfolio desde el panel ADMIN

--- pages/views.py
+++ pages/views.py
@@ -10,10 +10,6 @@ def about(request):
     return render(request, "about.html")
 
 
-def portfolio(request):
-    return render(request, "portfolio.html")
-
-
 def contact(request):
     return render(request, "contact.html")
 
